8c949d6071d55ef5b1d06ea06a00d08b4c278da9
AaronWilzbach <wilzbachaaron@gmail.com>
2024-11-20 22:20:25 -0600

    Body test 2

--- app.js
+++ app.js
@@ -14,18 +14,6 @@ const html = `
 <html>
   <head>
     <title>Welcome to Kappa Sigma</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
       @font-face {
         font-family: "neo-sans";
@@ -39,7 +27,10 @@ const html = `
         font-size: calc(62rem / 16);
       }
       body {
-        background: white;
+        background: #185012;
+        font-family: Arial, sans-serif;
+        display: flex;
+        align-items: center;
       }
       section {
         border-radius: 1em;
